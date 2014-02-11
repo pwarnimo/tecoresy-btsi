1c5c8461538edc580598af9f8e2b70802dc0f54a
pwarnimo <pwarnimo@gmail.com>
2014-02-11 15:11:40 +0100

    T61 -- Commit 8 : Prototype->Layout changed

--- prototype/admin/inc/pages/terrains.inc.php
+++ prototype/admin/inc/pages/terrains.inc.php
@@ -4,4 +4,75 @@ echo <<< PAGE
     <div class="page-header">
         <h1>Terrains <small>TECORESY Admin</small></h1>
     </div>
+
+    <table id="dtable">
+        <thead>
+            <tr>
+                <th style="width: 32px;"><input type="checkbox"></th>
+                <th>Terrain No°</th>
+                <th>Description</th>
+                <th>Available</th>
+                <th colspan="2"></th>
+            </tr>
+        </thead>
+
+        <tbody>
+            <tr>
+                <td><input type="checkbox"></td>
+                <td style="width: 100px;">01</td>
+                <td>Lorem ipsum</td>
+                <td style="width: 60px;" class="yes">Yes</td>
+                <td style="width: 20px;"><span class="glyphicon glyphicon-pencil"></span></td>
+                <td style="width: 20px;"><span class="glyphicon glyphicon-trash"></span></td>
+            </tr>
+            <tr>
+                <td><input type="checkbox"></td>
+                <td>02</td>
+                <td>Lorem ipsum</td>
+                <td class="yes">Yes</td>
+                <td><span class="glyphicon glyphicon-pencil"></span></td>
+                <td><span class="glyphicon glyphicon-trash"></span></td>
+            </tr>
+            <tr>
+                <td><input type="checkbox"></td>
+                <td>03</td>
+                <td>Lorem ipsum</td>
+                <td class="no">No</td>
+                <td><span class="glyphicon glyphicon-pencil"></span></td>
+                <td><span class="glyphicon glyphicon-trash"></span></td>
+            </tr>
+            <tr>
+                <td><input type="checkbox"></td>
+                <td>04</td>
+                <td>Lorem ipsum</td>
+                <td class="yes">Yes</td>
+                <td><span class="glyphicon glyphicon-pencil"></span></td>
+                <td><span class="glyphicon glyphicon-trash"></span></td>
+            </tr>
+            <tr>
+                <td><input type="checkbox"></td>
+                <td>05</td>
+                <td>Lorem ipsum</td>
+                <td class="yes">Yes</td>
+                <td><span class="glyphicon glyphicon-pencil"></span></td>
+                <td><span class="glyphicon glyphicon-trash"></span></td>
+            </tr>
+            <tr>
+                <td><input type="checkbox"></td>
+                <td>06</td>
+                <td>Lorem ipsum</td>
+                <td class="yes">Yes</td>
+                <td><span class="glyphicon glyphicon-pencil"></span></td>
+                <td><span class="glyphicon glyphicon-trash"></span></td>
+            </tr>
+            <tr>
+                <td><input type="checkbox"></td>
+                <td>07</td>
+                <td>Lorem ipsum</td>
+                <td class="yes">Yes</td>
+                <td><span class="glyphicon glyphicon-pencil"></span></td>
+                <td><span class="glyphicon glyphicon-trash"></span></td>
+            </tr>
+        </tbody>
+    </table>
 PAGE;
